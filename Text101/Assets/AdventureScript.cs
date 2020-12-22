7441bc794705f184ac30b4e3727de8a8e19d8a7f
Monica Lucarini <63265170+MonicaLuca@users.noreply.github.com>
2020-12-22 12:19:43 -0500

    toggle between states  through button press room 1, room 2, and starting states

--- Text101/Assets/AdventureScript.cs
+++ Text101/Assets/AdventureScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -23,6 +24,25 @@ public class AdventureScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
     }
 }
